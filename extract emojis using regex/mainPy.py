167e4cebcbb0ea3e7c84b8b0c5853ab0b409b6b3
unknown <syed.ammar.98sep@gmail.com>
2020-10-29 20:57:33 +0500

    extract emojis using regex

---
+++ extract emojis using regex/mainPy.py
@@ -0,0 +1,11 @@
+import re
+
+
+with open('test.txt','w',encoding='utf-8-sig') as f:
+    f.write('\U0001f44d')
+    f.write('\U0001F606')
+    f.write('\U0001F605')
+    f.write('\U0001F923')
+    f.write('\U0001F923')
+    f.write('\U0001F602')
+    f.write('\U0001F606')
